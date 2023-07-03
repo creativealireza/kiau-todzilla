94629f9211585fc48f37795c5e3462ddaaf6bd0b
Alireza Rahimi <ar.dvlpr@outlook.com>
2023-07-03 19:37:45 +0330

    Update App and index

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import { Header } from "./components/Header/Header";
-import { Footer } from "./components/Footer/Footer";
+import { useState } from 'react';
+import { Header } from "./components/Header/Header"
+import { Footer } from "./components/Footer/Footer"
 import { UITabs } from "./components/UITabs/UITabs";
+import { Authentication } from "./Authentication/Authentication";
+import { readData } from "./Tools/localActions";
+import { useSelector } from 'react-redux'
 import { LandingPage } from "./components/LandingPage/LandingPage";
+export default function App() {
+  const isAuth = useSelector((state) => state.auth.isAuth)
+  const [authDialog, setAuthDialog] = useState(false)
 
-import './App.css';
-
-
-function App() {
   return (
-    <div className="App">
-      <Header />
-      <UITabs />
-      <LandingPage />
+    <>
+      <Header setAuthDialog={setAuthDialog} auth={isAuth || readData('user')?.members?.length} />
+
+      {isAuth || readData('user')?.members?.length
+        ? <UITabs /> : authDialog
+          ? <Authentication isAuth setMainAuthDialog={setAuthDialog} /> : <LandingPage setAuthDialog={setAuthDialog} />}
+
       <Footer />
-    </div>
+    </>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
-
-import './index.css';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux'
+import { store } from "./store"
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-      <BrowserRouter>
+    <BrowserRouter>
+      <Provider store={store}>
         <App />
-      </BrowserRouter>
-    {/* </Provider> */}
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
