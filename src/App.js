0bbfc340d9de20dc763c3db2e3711f32bc6ce514
Alireza Rahimi <ar.dvlpr@outlook.com>
2023-07-03 18:23:30 +0330

    add header footer UITabs and LandingPage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+import { UITabs } from "./components/UITabs/UITabs";
+import { LandingPage } from "./components/LandingPage/LandingPage";
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <UITabs />
+      <LandingPage />
+      <Footer />
     </div>
   );
 }
