0090012b08f27298df11a8328d1f5c0b22a8cfe0
Alireza Rahimi <ar.dvlpr@outlook.com>
2023-07-31 23:41:57 +0330

    fix deleteAll bug

--- controller/taskController.js
+++ controller/taskController.js
@@ -13,7 +13,7 @@ const getTasks = async (req, res) => {
     if (!admin) return res.status(204).json({ message: "No admin found" });
 
     const tasks = [];
-    admin?.tasks?.map(task => {
+    admin?.tasks?.map((task) => {
       if (!task?.isDeleted) {
         tasks.push({
           title: task?.title,
@@ -22,10 +22,10 @@ const getTasks = async (req, res) => {
           isCompleted: task?.isCompleted,
           isEdited: task?.isEdited,
           isDeleted: task?.isDeleted,
-          taskId: task?._id.toString()
-        })
+          taskId: task?._id.toString(),
+        });
       }
-    })
+    });
 
     res.json(tasks);
   } catch (err) {
@@ -39,25 +39,24 @@ const getTasks = async (req, res) => {
 const createTask = async (req, res) => {
   const { adminId } = req?.body;
 
-  if (!adminId)
-    return res.status(400).json({ message: "adminId is required" });
+  if (!adminId) return res.status(400).json({ message: "adminId is required" });
 
   try {
     const admin = await Admin.findOne({ _id: adminId });
 
-    const historyObj = new History(
-      {
-        title: req?.body?.title,
-        type: "create",
-        date: new Date().toLocaleString(),
-        adminId: adminId,
-        members: [{
+    const historyObj = new History({
+      title: req?.body?.title,
+      type: "create",
+      date: new Date().toLocaleString(),
+      adminId: adminId,
+      members: [
+        {
           fullName: admin?.fullName,
           age: admin?.age,
           email: admin?.email,
-        }]
-      }
-    );
+        },
+      ],
+    });
 
     await historyObj.save();
 
@@ -65,19 +64,21 @@ const createTask = async (req, res) => {
 
     const task = await admin.save();
 
-    const tasksArray = task?.tasks?.map(task => {
-      if (!task?.isDeleted) {
-        return {
-          title: task?.title,
-          description: task?.title,
-          members: task?.members,
-          isCompleted: false,
-          isEdited: false,
-          isDeleted: false,
-          taskId: task?._id.toString(),
+    const tasksArray = task?.tasks
+      ?.map((task) => {
+        if (!task?.isDeleted) {
+          return {
+            title: task?.title,
+            description: task?.title,
+            members: task?.members,
+            isCompleted: false,
+            isEdited: false,
+            isDeleted: false,
+            taskId: task?._id.toString(),
+          };
         }
-      }
-    }).filter(Boolean)
+      })
+      .filter(Boolean);
 
     res.json(tasksArray);
   } catch (err) {
@@ -90,55 +91,62 @@ const createTask = async (req, res) => {
 // @access - public
 const editTask = async (req, res) => {
   const editedTask = req?.body;
-  if (!editedTask?.adminId) return res.status(400).json({ message: "adminId parameter is required" });
+  if (!editedTask?.adminId)
+    return res.status(400).json({ message: "adminId parameter is required" });
 
   try {
     const admin = await Admin.findOne({ _id: editedTask?.adminId });
     if (!admin)
-      return res.status(204).json({ message: `No matches admin with id:${editedTask?.adminId}` });
+      return res
+        .status(204)
+        .json({ message: `No matches admin with id:${editedTask?.adminId}` });
 
-    const historyObj = new History(
-      {
-        title: req?.body?.title,
-        type: "edit",
-        date: new Date().toLocaleString(),
-        adminId: editedTask?.adminId,
-        members: [{
+    const historyObj = new History({
+      title: req?.body?.title,
+      type: "edit",
+      date: new Date().toLocaleString(),
+      adminId: editedTask?.adminId,
+      members: [
+        {
           fullName: admin?.fullName,
           age: admin?.age,
           email: admin?.email,
-        }]
-      }
-    );
+        },
+      ],
+    });
 
     await historyObj.save();
 
-    admin?.tasks
-      ?.forEach(oldTask => {
-        if (oldTask?._id?.toString() === editedTask?.taskId) {
-          oldTask.title = editedTask?.title;
-          oldTask.description = editedTask?.description;
-          oldTask.isEdited = editedTask?.isEdited;
-          oldTask.members = editedTask?.members;
-        }
-      })
+    admin?.tasks?.forEach((oldTask) => {
+      if (oldTask?._id?.toString() === editedTask?.taskId) {
+        oldTask.title = editedTask?.title;
+        oldTask.description = editedTask?.description;
+        oldTask.isEdited = editedTask?.isEdited;
+        oldTask.members = editedTask?.members;
+      }
+    });
 
     const savedAdmin = await admin.save();
 
     const newTasks = [];
-    savedAdmin?.tasks?.map(task => {
+    savedAdmin?.tasks?.map((task) => {
       if (!task?.isDeleted) {
-        newTasks.push(Object.assign({}, {
-          taskId: task?._id?.toString(),
-          title: task?.title,
-          description: task?.description,
-          isCompleted: task?.isCompleted,
-          isDeleted: task?.isDeleted,
-          isEdited: task?.isEdited,
-          members: task?.members,
-        }))
+        newTasks.push(
+          Object.assign(
+            {},
+            {
+              taskId: task?._id?.toString(),
+              title: task?.title,
+              description: task?.description,
+              isCompleted: task?.isCompleted,
+              isDeleted: task?.isDeleted,
+              isEdited: task?.isEdited,
+              members: task?.members,
+            }
+          )
+        );
       }
-    })
+    });
 
     const result = {
       fullName: savedAdmin?.fullName,
@@ -168,57 +176,70 @@ const editTask = async (req, res) => {
 // @access - public
 const completeTask = async (req, res) => {
   const { adminId, taskId, userId } = req?.body;
-  if (!adminId || !taskId) return res.status(400).json({ message: "adminId and taskId are both required" });
+  if (!adminId || !taskId)
+    return res
+      .status(400)
+      .json({ message: "adminId and taskId are both required" });
 
   try {
     const admin = await Admin.findOne({ _id: adminId });
     if (!admin)
-      return res.status(204).json({ message: `no matches admin with id:${adminId}` });
+      return res
+        .status(204)
+        .json({ message: `no matches admin with id:${adminId}` });
 
     const history = [];
-    admin?.tasks?.map(task => {
+    admin?.tasks?.map((task) => {
       if (task?._id?.toString() === taskId) {
-        const member = task?.members?.filter(member => member?.memberId === userId);
+        const member = task?.members?.filter(
+          (member) => member?.memberId === userId
+        );
         history.push({
           title: task?.title,
-          type: 'complete',
+          type: "complete",
           adminId: adminId,
           date: new Date().toLocaleString(),
-          members: member?.length ? member : [{
-            fullName: admin?.fullName,
-            age: admin?.age,
-            email: admin?.email
-          }],
-        })
+          members: member?.length
+            ? member
+            : [
+                {
+                  fullName: admin?.fullName,
+                  age: admin?.age,
+                  email: admin?.email,
+                },
+              ],
+        });
       }
-    })
+    });
 
-    const historyObj = new History(
-      history[0]
-    );
+    const historyObj = new History(history[0]);
 
     await historyObj.save();
 
-    admin?.tasks
-      ?.forEach(task => {
-        if (task?._id?.toString() === taskId) task.isCompleted = true;
-      })
+    admin?.tasks?.forEach((task) => {
+      if (task?._id?.toString() === taskId) task.isCompleted = true;
+    });
     const completedTaskAdmin = await admin.save();
 
     const newTasks = [];
-    completedTaskAdmin?.tasks?.map(task => {
+    completedTaskAdmin?.tasks?.map((task) => {
       if (!task?.isDeleted) {
-        newTasks.push(Object.assign({}, {
-          taskId: task?._id?.toString(),
-          title: task?.title,
-          description: task?.description,
-          isCompleted: task?.isCompleted,
-          isDeleted: task?.isDeleted,
-          isEdited: task?.isEdited,
-          members: task?.members,
-        }))
+        newTasks.push(
+          Object.assign(
+            {},
+            {
+              taskId: task?._id?.toString(),
+              title: task?.title,
+              description: task?.description,
+              isCompleted: task?.isCompleted,
+              isDeleted: task?.isDeleted,
+              isEdited: task?.isEdited,
+              members: task?.members,
+            }
+          )
+        );
       }
-    })
+    });
 
     const result = {
       fullName: completedTaskAdmin?.fullName,
@@ -248,57 +269,70 @@ const completeTask = async (req, res) => {
 // @access - public
 const revertTask = async (req, res) => {
   const { adminId, taskId, userId } = req?.body;
-  if (!adminId || !taskId) return res.status(400).json({ message: "adminId and taskId are both required" });
+  if (!adminId || !taskId)
+    return res
+      .status(400)
+      .json({ message: "adminId and taskId are both required" });
 
   try {
     const admin = await Admin.findOne({ _id: adminId });
     if (!admin)
-      return res.status(204).json({ message: `no matches admin with id:${adminId}` });
+      return res
+        .status(204)
+        .json({ message: `no matches admin with id:${adminId}` });
 
     const history = [];
-    admin?.tasks?.map(task => {
+    admin?.tasks?.map((task) => {
       if (task?._id?.toString() === taskId) {
-        const member = task?.members?.filter(member => member?.memberId === userId);
+        const member = task?.members?.filter(
+          (member) => member?.memberId === userId
+        );
         history.push({
           title: task?.title,
-          type: 'revert',
+          type: "revert",
           adminId: adminId,
           date: new Date().toLocaleString(),
-          members: member?.length ? member : [{
-            fullName: admin?.fullName,
-            age: admin?.age,
-            email: admin?.email
-          }],
-        })
+          members: member?.length
+            ? member
+            : [
+                {
+                  fullName: admin?.fullName,
+                  age: admin?.age,
+                  email: admin?.email,
+                },
+              ],
+        });
       }
-    })
+    });
 
-    const historyObj = new History(
-      history[0]
-    );
+    const historyObj = new History(history[0]);
 
     await historyObj.save();
 
-    admin?.tasks
-      ?.forEach(task => {
-        if (task?._id?.toString() === taskId) task.isCompleted = false;
-      })
+    admin?.tasks?.forEach((task) => {
+      if (task?._id?.toString() === taskId) task.isCompleted = false;
+    });
     const revertedTaskAdmin = await admin.save();
 
     const newTasks = [];
-    revertedTaskAdmin?.tasks?.map(task => {
+    revertedTaskAdmin?.tasks?.map((task) => {
       if (!task?.isDeleted) {
-        newTasks.push(Object.assign({}, {
-          taskId: task?._id?.toString(),
-          title: task?.title,
-          description: task?.description,
-          isCompleted: task?.isCompleted,
-          isDeleted: task?.isDeleted,
-          isEdited: task?.isEdited,
-          members: task?.members,
-        }))
+        newTasks.push(
+          Object.assign(
+            {},
+            {
+              taskId: task?._id?.toString(),
+              title: task?.title,
+              description: task?.description,
+              isCompleted: task?.isCompleted,
+              isDeleted: task?.isDeleted,
+              isEdited: task?.isEdited,
+              members: task?.members,
+            }
+          )
+        );
       }
-    })
+    });
 
     const result = {
       fullName: revertedTaskAdmin?.fullName,
@@ -334,29 +368,34 @@ const deleteAllTasks = async (req, res) => {
   try {
     const admin = await Admin.findOne({ _id: adminId });
     if (!admin)
-      return res.status(204).json({ message: `no matches admin with id:${adminId}` });
+      return res
+        .status(204)
+        .json({ message: `no matches admin with id:${adminId}` });
 
-    const member = task?.members?.filter(member => member?.memberId === userId);
-    const historyObj = new History(
-      {
-        title: 'All Tasks',
-        type: 'deleteAll',
-        adminId: adminId,
-        date: new Date().toLocaleString(),
-        members: member?.length ? member : [{
-          fullName: admin?.fullName,
-          age: admin?.age,
-          email: admin?.email
-        }],
-      }
+    const member = admin?.members?.filter(
+      (member) => member?.memberId === userId
     );
+    const historyObj = new History({
+      title: "All Tasks",
+      type: "deleteAll",
+      adminId: adminId,
+      date: new Date().toLocaleString(),
+      members: member?.length
+        ? member
+        : [
+            {
+              fullName: admin?.fullName,
+              age: admin?.age,
+              email: admin?.email,
+            },
+          ],
+    });
 
     await historyObj.save();
 
-    admin?.tasks
-      ?.forEach(task => {
-        task.isDeleted = true;
-      })
+    admin?.tasks?.forEach((task) => {
+      task.isDeleted = true;
+    });
     const newAdmin = await admin.save();
 
     const result = {
@@ -392,46 +431,56 @@ const deleteCompletedTasks = async (req, res) => {
   try {
     const admin = await Admin.findOne({ _id: adminId });
     if (!admin)
-      return res.status(204).json({ message: `no matches admin with id:${adminId}` });
+      return res
+        .status(204)
+        .json({ message: `no matches admin with id:${adminId}` });
 
-    const member = task?.members?.filter(member => member?.memberId === userId);
-    const historyObj = new History(
-      {
-        title: 'All Completed Tasks',
-        type: 'deleteAllCompleted',
-        adminId: adminId,
-        date: new Date().toLocaleString(),
-        members: member?.length ? member : [{
-          fullName: admin?.fullName,
-          age: admin?.age,
-          email: admin?.email
-        }],
-      }
+    const member = admin?.members?.filter(
+      (member) => member?.memberId === userId
     );
+    const historyObj = new History({
+      title: "All Completed Tasks",
+      type: "deleteAllCompleted",
+      adminId: adminId,
+      date: new Date().toLocaleString(),
+      members: member?.length
+        ? member
+        : [
+            {
+              fullName: admin?.fullName,
+              age: admin?.age,
+              email: admin?.email,
+            },
+          ],
+    });
 
     await historyObj.save();
 
-    admin?.tasks
-      ?.forEach(task => {
-        if (task?.isCompleted === true) task.isDeleted = true;
-      })
+    admin?.tasks?.forEach((task) => {
+      if (task?.isCompleted === true) task.isDeleted = true;
+    });
 
     const newAdmin = await admin.save();
 
     const newTasks = [];
-    newAdmin?.tasks?.map(task => {
+    newAdmin?.tasks?.map((task) => {
       if (!task?.isDeleted) {
-        newTasks.push(Object.assign({}, {
-          taskId: task?._id?.toString(),
-          title: task?.title,
-          description: task?.description,
-          isCompleted: task?.isCompleted,
-          isDeleted: task?.isDeleted,
-          isEdited: task?.isEdited,
-          members: task?.members,
-        }))
+        newTasks.push(
+          Object.assign(
+            {},
+            {
+              taskId: task?._id?.toString(),
+              title: task?.title,
+              description: task?.description,
+              isCompleted: task?.isCompleted,
+              isDeleted: task?.isDeleted,
+              isEdited: task?.isEdited,
+              members: task?.members,
+            }
+          )
+        );
       }
-    })
+    });
 
     const result = {
       fullName: newAdmin?.fullName,
@@ -462,56 +511,69 @@ const deleteCompletedTasks = async (req, res) => {
 const deleteTask = async (req, res) => {
   const { adminId, taskId, userId } = req?.body;
 
-  if (!adminId || !taskId) return res.status(400).json({ message: "adminId and taskId are both required" });
+  if (!adminId || !taskId)
+    return res
+      .status(400)
+      .json({ message: "adminId and taskId are both required" });
 
   try {
     const admin = await Admin.findOne({ _id: adminId });
     if (!admin)
-      return res.status(204).json({ message: `no matches admin with id:${adminId}` });
+      return res
+        .status(204)
+        .json({ message: `no matches admin with id:${adminId}` });
     const history = [];
-    admin?.tasks?.map(task => {
+    admin?.tasks?.map((task) => {
       if (task?._id?.toString() === taskId) {
-        const member = task?.members?.filter(member => member?.memberId === userId);
+        const member = task?.members?.filter(
+          (member) => member?.memberId === userId
+        );
         history.push({
           title: task?.title,
-          type: 'deleteOne',
+          type: "deleteOne",
           adminId: adminId,
           date: new Date().toLocaleString(),
-          members: member?.length ? member : [{
-            fullName: admin?.fullName,
-            age: admin?.age,
-            email: admin?.email
-          }],
-        })
+          members: member?.length
+            ? member
+            : [
+                {
+                  fullName: admin?.fullName,
+                  age: admin?.age,
+                  email: admin?.email,
+                },
+              ],
+        });
       }
-    })
+    });
 
-    const historyObj = new History(
-      history[0]
-    );
+    const historyObj = new History(history[0]);
 
     await historyObj.save();
 
-    admin?.tasks
-      ?.forEach(task => {
-        if (task?._id?.toString() === taskId) task.isDeleted = true;
-      })
+    admin?.tasks?.forEach((task) => {
+      if (task?._id?.toString() === taskId) task.isDeleted = true;
+    });
     const deletedTaskAdmin = await admin.save();
 
     const newTasks = [];
-    deletedTaskAdmin?.tasks?.map(task => {
+    deletedTaskAdmin?.tasks?.map((task) => {
       if (!task?.isDeleted) {
-        newTasks.push(Object.assign({}, {
-          taskId: task?._id?.toString(),
-          title: task?.title,
-          description: task?.description,
-          isCompleted: task?.isCompleted,
-          isDeleted: task?.isDeleted,
-          isEdited: task?.isEdited,
-          members: task?.members,
-        }))
+        newTasks.push(
+          Object.assign(
+            {},
+            {
+              taskId: task?._id?.toString(),
+              title: task?.title,
+              description: task?.description,
+              isCompleted: task?.isCompleted,
+              isDeleted: task?.isDeleted,
+              isEdited: task?.isEdited,
+              members: task?.members,
+            }
+          )
+        );
       }
-    })
+    });
 
     const result = {
       fullName: deletedTaskAdmin?.fullName,
@@ -544,5 +606,5 @@ module.exports = {
   editTask,
   revertTask,
   deleteAllTasks,
-  deleteCompletedTasks
+  deleteCompletedTasks,
 };
